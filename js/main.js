da9131ffbfc9519431f04999ebe8045cae3b7c1e
eliorpacarada1 <eliorpacarada1@gmail.com>
2021-01-31 23:30:46 +0100

    front-end-for-springboot-app

--- js/main.js
+++ js/main.js
@@ -1,40 +1,28 @@
-function toggleMenu() {
-    var x = document.getElementById("test");
-
-
-     if (x.style.display === "none") {
-        x.style.display = "block";
-        document.getElementById("main-id").style.width = "80%";
-    } else {
-        x.style.display = "none";
-        document.getElementById("main-id").style.width = "100%";
-    }
-
-}
-
 $('#singleUploadForm').submit(function(event) {
     var formElement = this;
     // You can directly create form data from the form element
     // (Or you could get the files from input element and append them to FormData as we did in vanilla javascript)
     var formData = new FormData(formElement);
-
+    console.log("HELLO")
     $.ajax({
         type: "POST",
         enctype: 'multipart/form-data',
-        url: "http://localhost:8081/upload",
+        url: "http://localhost:8080/upload",
         data: formData,
         processData: false,
         contentType: false,
         success: function (response) {
+            console.log("HI");
             console.log(response);
             singleFileUploadSuccess.innerHTML = "<p>File Uploaded Successfully.</p> <p>DownloadUrl : <a href='" + response.url+ "' target='_blank'>" + response.url + "</a></p> <p>Name of the file: </p>" + response.name;
 
         },
         error: function (error) {
+            console.log("HI2");
             console.log(error);
             // process error
         }
     });
-
+    console.log("HI3");
     event.preventDefault();
 });
